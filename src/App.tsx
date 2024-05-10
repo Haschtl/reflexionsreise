ad7a10376a515b436c5473a7cd8d8f5c599f14cb
haschtl <haschtl@pm.me>
2024-05-10 21:11:26 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -249,7 +249,7 @@ function App() {
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
-                allowFullscreen
+                allowFullScreen
               ></iframe>
             </div>
           </CarouselPage>
